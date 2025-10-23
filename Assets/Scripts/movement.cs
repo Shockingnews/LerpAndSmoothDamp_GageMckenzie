9fc87596740512c4442c659e3ba7bc8f9c9dff11
Gage Mckenzie <gagemckenzie70@gmail.com>
2025-10-23 10:55:01 -0300

    small fixes

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,7 +3,7 @@ using UnityEngine.UIElements;
 
 public class movement : MonoBehaviour
 {
-    Vector3 startPos;
+    Vector3 startPosition;
     static float timer = 0;
     public float duration = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,7 +19,7 @@ public class movement : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        transform.position = Vector3.Lerp(startPos, startPos + Vector3.right * 10, timer / duration);
+        transform.position = Vector3.Lerp(startPosition, startPosition + Vector3.right * 10, timer / duration);
         if (timer > duration)
         {
             timer = 0;
